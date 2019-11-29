5b91a6173245f3350447720cc8cf7521fc0ac499
Rafael Montano Boraita <rmn@novatec-gmbh.de>
2019-11-29 09:33:37 +0100

    Check when a translation is null

--- lib/native_i18n_flutter_plugin.dart
+++ lib/native_i18n_flutter_plugin.dart
@@ -8,20 +8,23 @@ import 'package:native_i18n_flutter_plugin/translation.dart';
 
 /// Plugin that fetches translation using Native API.
 class NativeI18nFlutterPlugin {
-  static const MethodChannel _channel = const MethodChannel('native_i18n_flutter_plugin');
+  static const MethodChannel _channel =
+  const MethodChannel('native_i18n_flutter_plugin');
   static var _logger = Logger();
   static var cache = SimpleCache(storage: new SimpleStorage(size: 100));
 
   /// Get a single translation
   static Future<String> getTranslation(Translation translation) {
     Completer<String> translationCompletable = Completer();
-    getTranslations([translation]).then((map) => translationCompletable.complete(map[translation.translationKey]));
+    getTranslations([translation]).then((map) =>
+        translationCompletable.complete(map[translation.translationKey]));
 
     return translationCompletable.future;
   }
 
   /// Get a map of translation where the key is the translation key and its translation.
-  static Future<Map<String, String>> getTranslations(List<Translation> translations) {
+  static Future<Map<String, String>> getTranslations(
+      List<Translation> translations) {
     Completer<Map<String, String>> translationsCompletable = Completer();
     Map<String, Translation> translationMap = {};
     Map<String, String> translationCompleted = {};
@@ -43,11 +46,18 @@ class NativeI18nFlutterPlugin {
       _logger.d("Translating $translationMap");
       _channel //
           .invokeMapMethod(
-              'getTranslations', jsonEncode(translations.where((i) => i != null).map((i) => i.toMap).toList()))
+          'getTranslations',
+          jsonEncode(translations
+              .where((i) => i != null)
+              .map((i) => i.toMap)
+              .toList()))
           .then((map) {
         map.forEach((translationKey, translation) {
-          cache.set(translationKey, translation);
-          translationCompleted[translationKey] = translationMap[translationKey].format(translation);
+          if (translationMap[translationKey] != null) {
+            cache.set(translationKey, translation);
+            translationCompleted[translationKey] =
+                translationMap[translationKey].format(translation);
+          }
         });
         translationsCompletable.complete(translationCompleted);
       });
